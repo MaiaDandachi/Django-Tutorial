efbe4519d28c1062dd900a513b515fda71b4741f
Maia-Dandachi <Lynn.dandachi@bilgiedu.net>
2022-01-26 20:15:27 +0300

    Implemented a Create article HTML form (Template & View)

--- articles/views.py
+++ articles/views.py
@@ -24,6 +24,19 @@ def article_search_view(request):
     return render(request, "articles/search.html", context=context)
 
 
+def article_create_view(request):
+    # print(request.POST)
+    context = {}
+    if request.method == "POST":
+        title = request.POST.get("title")
+        content = request.POST.get("content")
+        print(title, content)
+        article_object = Article.objects.create(title=title, content=content)
+        context['object'] = article_object
+        context['created'] = True
+    return render(request, "articles/create.html", context=context)
+
+
 def article_detail_view(request, id=None):
     article_obj = None
     if id is not None:
--- djangoTutorial/urls.py
+++ djangoTutorial/urls.py
@@ -22,6 +22,7 @@ from .views import home_view
 urlpatterns = [
     path('', home_view),
     path('articles/', views.article_search_view),
+    path('articles/create/', views.article_create_view),
     path('articles/<int:id>/', views.article_detail_view),
     path('admin/', admin.site.urls),
 ]
